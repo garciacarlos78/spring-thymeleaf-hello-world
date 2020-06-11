249524df5fda8afe0991d4c67edc39663006eadd
Carlos Garcia Romero <garciacarlos78@gmail.com>
2020-06-11 13:23:59 +0200

    Simple hello world

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # spring-thymeleaf-hello-world
 Simple spring-thymeleaf hello world projects 
+
+## spring-thymeleaf-hello-world
+Simple spring boot thymeleaf hello world project.  
+Endpoint: localhost:8080/hello  
+Response: "Hello World, server time: *server time*"
+
---
+++ spring-thymeleaf-hello-world/src/test/java/com/cgrdev/springthymeleafhelloworld/SpringThymeleafHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cgrdev.springthymeleafhelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringThymeleafHelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
