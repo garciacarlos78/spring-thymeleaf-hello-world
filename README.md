982552a3f1b7f08f3c7f8284ccc7f2930056e18e
Carlos Garcia Romero <garciacarlos78@gmail.com>
2020-06-11 14:08:51 +0200

    spring-thymeleaf-hello-world-css

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ Simple spring boot thymeleaf hello world project.
 Endpoint: localhost:8080/hello  
 Response: "Hello World, server time: *server time*"
 
+## spring-thymeleaf-hello-world-css
+Added css style to spring-thymeleaf-hello-world project.  
+Added as local resource (css file in project).
---
+++ spring-thymeleaf-hello-world-css/src/main/java/com/cgrdev/springthymeleafhelloworld/SpringThymeleafHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.cgrdev.springthymeleafhelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringThymeleafHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringThymeleafHelloWorldApplication.class, args);
+	}
+
+}
